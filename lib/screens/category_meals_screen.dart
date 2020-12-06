cdb1807e93d1de5450e2b98725d95b84dba1c6b2
Adam Weil <adamweil@Adams-MacBook-Pro.local>
2020-12-05 19:24:18 -0500

    finished laying out and populating the meals page

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -32,6 +32,7 @@ class CategoryMealsScreen extends StatelessWidget {
         itemBuilder: (ctx, index) {
           // return Text(categoryMeals[index].title),
           return MealItem(
+            id: categoryMeals[index].id,
             title: categoryMeals[index].title,
             imageUrl: categoryMeals[index].imageUrl,
             duration: categoryMeals[index].duration,
